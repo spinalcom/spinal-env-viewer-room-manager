bd87201fe6fe502cde4959aab78351bbc96c051e
LorisC <loris@ciprin.fr>
2019-05-27 10:03:59 +0200

    add multi model support

--- js/utilities.js
+++ js/utilities.js
@@ -16,41 +16,42 @@ let ItemsColoredMap = new Map();
 let BimElementsColor = new Map();
 
 let utilities = {
-  colorElement(node, isColored) {
+  colorElement( node, isColored ) {
     let id = node.id.get();
-
-    return this.getNodesToColor(node).then(res => {
-      res.nodes.forEach(el => {
+    
+    return this.getNodesToColor( node ).then( res => {
+      res.nodes.forEach( el => {
         if (!isColored) {
-          this.colorBimObjectGroup(id, el, res.relationName, res
-            .color);
+          this.colorBimObjectGroup( id, el, res.relationName, res
+            .color );
         } else {
-          this.restaureBimObjectColor(id, el, res.relationName);
+          this.restaureBimObjectColor( id, el, res.relationName );
         }
-      });
-    });
+      } );
+    } );
   },
-  restaureBimObjectColor(id, node, relationName) {
-    return SpinalGraphService.getChildren(node.id.get(), [relationName]).then(
+  restaureBimObjectColor( id, node, relationName ) {
+    return SpinalGraphService.getChildren( node.id.get(), [relationName] ).then(
       children => {
         //   ItemsColoredMap.delete(node.id.get());
-        ItemsColoredMap.delete(id);
-
-        children.forEach(child => {
-
+        ItemsColoredMap.delete( id );
+        
+        children.forEach( child => {
+          
           window.v.setThemingColor(
             child.dbid.get(),
             // eslint-disable-next-line no-undef
-            new THREE.Vector4(0, 0, 0, 0)
+            new THREE.Vector4( 0, 0, 0, 0 ),
+            window.spinal.AssemblyManagerService._getCurrentModel()
           );
-
-          let allColors = BimElementsColor.get(child.dbid.get());
-
+          
+          let allColors = BimElementsColor.get( child.dbid.get() );
+          
           if (allColors) {
             //   allColors = allColors.filter(el => el.id !== node.id.get());
-            allColors = allColors.filter(el => el.id !== id);
-            BimElementsColor.set(child.dbid.get(), allColors);
-
+            allColors = allColors.filter( el => el.id !== id );
+            BimElementsColor.set( child.dbid.get(), allColors );
+            
             if (allColors.length > 0) {
               let color = allColors[0].color;
               window.v.setThemingColor(
@@ -61,68 +62,69 @@ let utilities = {
                   color.g / 255,
                   color.b / 255,
                   0.7
-                )
+                ),
+                window.spinal.AssemblyManagerService._getCurrentModel()
               );
             }
           }
-        });
+        } );
       }
     );
   },
-  colorBimObjectGroup(id, node, relationName, argColor) {
-    return SpinalGraphService.getChildren(node.id.get(), [relationName]).then(
+  colorBimObjectGroup( id, node, relationName, argColor ) {
+    return SpinalGraphService.getChildren( node.id.get(), [relationName] ).then(
       children => {
         let nodeColor;
-
+        
         if (argColor) {
           nodeColor = argColor;
         } else {
           nodeColor = node.color ? node.color.get() : "#000000";
         }
-
-        let color = this.convertHexColorToRGB(nodeColor);
-
-        children.forEach(child => {
+        
+        let color = this.convertHexColorToRGB( nodeColor );
+        
+        children.forEach( child => {
           //   ItemsColoredMap.set(node.id.get(), node.id.get());
-          ItemsColoredMap.set(id, id);
-
-          let BimColors = BimElementsColor.get(child.dbid.get()) ?
-            BimElementsColor.get(child.dbid.get()) : [];
-
-          BimColors.push({
+          ItemsColoredMap.set( id, id );
+          
+          let BimColors = BimElementsColor.get( child.dbid.get() ) ?
+            BimElementsColor.get( child.dbid.get() ) : [];
+          
+          BimColors.push( {
             id: id, //node.id.get(),
             color: color
-          });
-
-          BimElementsColor.set(child.dbid.get(), BimColors);
-
+          } );
+          
+          BimElementsColor.set( child.dbid.get(), BimColors );
+          
           window.v.setThemingColor(
             child.dbid.get(),
             // eslint-disable-next-line no-undef
-            new THREE.Vector4(color.r / 255, color.g / 255, color.b /
-              255, 0.7)
+            new THREE.Vector4( color.r / 255, color.g / 255, color.b /
+              255, 0.7 ), window.spinal.AssemblyManagerService._getCurrentModel()
           );
-        });
+        } );
       }
     );
   },
-  convertHexColorToRGB(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  convertHexColorToRGB( hex ) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec( hex );
     return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
+        r: parseInt( result[1], 16 ),
+        g: parseInt( result[2], 16 ),
+        b: parseInt( result[3], 16 )
       } :
       null;
   },
-  async getNodesToColor(node) {
+  async getNodesToColor( node ) {
     let type = node.type.get();
     let res = {};
     if (type === ROOMS_GROUP) {
       res["color"] = node.color.get();
-      res["nodes"] = await SpinalGraphService.getChildren(node.id.get(), [
+      res["nodes"] = await SpinalGraphService.getChildren( node.id.get(), [
         ROOMS_TO_ELEMENT_RELATION
-      ]);
+      ] );
       res["relationName"] = geographicService.constants.EQUIPMENT_RELATION;
     } else if (type === EQUIPMENTS_GROUP) {
       res["nodes"] = [node];
@@ -130,17 +132,17 @@ let utilities = {
     }
     return res;
   },
-  colorContext(context, isColored) {
+  colorContext( context, isColored ) {
     let relationName =
       context.type.get() === ROOMS_GROUP_CONTEXT ?
-      ROOMS_GROUP_RELATION :
-      EQUIPMENTS_GROUP_RELATION;
-
-    SpinalGraphService.getChildren(context.id.get(), [relationName]).then(
+        ROOMS_GROUP_RELATION :
+        EQUIPMENTS_GROUP_RELATION;
+    
+    SpinalGraphService.getChildren( context.id.get(), [relationName] ).then(
       children => {
-        children.forEach(child => {
-          this.colorElement(child, isColored);
-        });
+        children.forEach( child => {
+          this.colorElement( child, isColored );
+        } );
       }
     );
   }
