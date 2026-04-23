642d93b7b40881c76e0956f7e9ff0b926906d1bf
mbt-tech <mbaradji221@outlook.fr>
2026-04-23 13:15:51 +0200

    add documentary group

--- js/types.js
+++ js/types.js
@@ -5,64 +5,69 @@ import { EVENT_TYPE } from "spinal-env-viewer-task-service";
 import { spinalControlPointService } from 'spinal-env-viewer-plugin-control-endpoint-service';
 // import { spinalAnalyticService } from "spinal-env-viewer-plugin-analytics-service";
 
-export default [{
-  name: "Geographic Context Group",
-  type: CONTEXT_TYPE
-},
-{
-  name: "Site Group",
-  type: SITE_TYPE
-},
-{
-  name: "Building Group",
-  type: BUILDING_TYPE
-},
-{
-  name: "Floor Group",
-  type: FLOOR_TYPE
-},
-{
-  name: "Zone Group",
-  type: ZONE_TYPE
-},
-{
-  name: "Rooms Group",
-  type: ROOM_TYPE,
-},
-{
-  name: "Equipments Group",
-  type: EQUIPMENT_TYPE,
-},
-{
-  name: "Endpoint Group",
-  type: SpinalBmsEndpoint.nodeTypeName,
-},
-{
-  name: "Device Group",
-  type: SpinalBmsDevice.nodeTypeName
-},
-{
-  name: "Network Group",
-  type: SpinalBmsNetwork.nodeTypeName
-},
-{
-  name: "EndpointGroup Group",
-  type: SpinalBmsEndpointGroup.nodeTypeName
-},
-{
-  name: "Note Group",
-  type: NOTE_TYPE
-},
-{
-  name: "Agenda Group",
-  type: EVENT_TYPE
-},
-{
-  name: "Control Points group",
-  type: spinalControlPointService.CONTROL_POINT_TYPE
-},
-{
-  name: "Analytics group",
-  type: "Analytic"// spinalAnalyticService.nodeType
-}
+export default [
+  {
+    name: "Geographic Context Group",
+    type: CONTEXT_TYPE
+  },
+  {
+    name: "Site Group",
+    type: SITE_TYPE
+  },
+  {
+    name: "Building Group",
+    type: BUILDING_TYPE
+  },
+  {
+    name: "Floor Group",
+    type: FLOOR_TYPE
+  },
+  {
+    name: "Zone Group",
+    type: ZONE_TYPE
+  },
+  {
+    name: "Rooms Group",
+    type: ROOM_TYPE,
+  },
+  {
+    name: "Equipments Group",
+    type: EQUIPMENT_TYPE,
+  },
+  {
+    name: "Endpoint Group",
+    type: SpinalBmsEndpoint.nodeTypeName,
+  },
+  {
+    name: "Device Group",
+    type: SpinalBmsDevice.nodeTypeName
+  },
+  {
+    name: "Network Group",
+    type: SpinalBmsNetwork.nodeTypeName
+  },
+  {
+    name: "EndpointGroup Group",
+    type: SpinalBmsEndpointGroup.nodeTypeName
+  },
+  {
+    name: "Note Group",
+    type: NOTE_TYPE
+  },
+  {
+    name: "Agenda Group",
+    type: EVENT_TYPE
+  },
+  {
+    name: "Control Points group",
+    type: spinalControlPointService.CONTROL_POINT_TYPE
+  },
+  {
+    name: "Analytics group",
+    type: "Analytic"// spinalAnalyticService.nodeType
+  },
+  {
+    name: "Documentary Group",
+    type: "Documentary"
+  }
 ];
